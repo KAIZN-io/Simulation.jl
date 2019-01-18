b9588ee8f1f85983ee91a3c4a828d6971e62918b
Jan-Luca Klees <email@janlucaklees.de>
2019-01-18 18:23:59 +0100

    Started translating the db-structure into SQL-Alchemy ORM
    With this commit I made efforts to translate the plain SQL code into
    SQL-Alchemy ORM code.
    
    The biggest change here is, that we no longer have each table be created
    once for each model, but now having only one set of tables for all
    models. To distinguish which model an entry belongs to I added a `model`
    column to each table that saves just that. This also eleminates the need
    for using schemas to organise tables.

--- createDatabaseStructure.py
+++ createDatabaseStructure.py
@@ -1,176 +1,115 @@
-"""this script creates the database storage structure"""
-
-exec(open("SYSTEM/py_packages.py").read())
-
-conn = psycopg2.connect(
-    host='db_postgres',
-    user='postgres',
-    dbname='simulation_results'
-)
-
-cur = conn.cursor()
-
-allModels_list = ['ion', 'hog', 'volume', 'combined_models']
-
-for NameOfModel in allModels_list:
-
-    """check if the model structure is already created"""
-    cur.execute("""
-            SELECT schema_name 
-            FROM information_schema.schemata 
-            WHERE schema_name = %s;
-            """, [NameOfModel])
-
-    if (len(cur.fetchall())>0) == False:
-
-        """create model terms schema"""
-        cur.execute(sql.SQL("""
-                CREATE SCHEMA {0}
-                    AUTHORIZATION postgres;
-                """).format(sql.Identifier(NameOfModel+'_terms')))
-
-        conn.commit()
-
-        """create schema"""
-        cur.execute(sql.SQL("""
-                CREATE SCHEMA {0}
-                    AUTHORIZATION postgres;
-                """).format(sql.Identifier(NameOfModel)))
-
-        conn.commit()
-
-        """create json table"""
-        cur.execute(sql.SQL("""
-                CREATE TABLE {0}.json
-                (
-                    seq serial,
-                    model_version json,
-                    adding_changes json,
-                    deleting_changes json,
-                    CONSTRAINT json_pkey PRIMARY KEY(seq)
-                )
-                WITH(
-                    OIDS=FALSE
-                )
-                TABLESPACE pg_default;
-                """).format(sql.Identifier(NameOfModel)))
-
-        conn.commit()
-
-        """create ex table"""
-        cur.execute(sql.SQL("""
-                CREATE TABLE {0}.ex
-                (
-                    studyid text COLLATE pg_catalog."default",
-                    domain text COLLATE pg_catalog."default",
-                    usubjid text COLLATE pg_catalog."default" NOT NULL,
-                    exseq integer NOT NULL,
-                    excat text COLLATE pg_catalog."default",
-                    extrt text COLLATE pg_catalog."default" NOT NULL,
-                    exdose real,
-                    exdosu text COLLATE pg_catalog."default",
-                    exstdtc_array double precision[],
-                    simulation_start double precision,
-                    simulation_stop double precision,
-                    co text COLLATE pg_catalog."default",
-                    modelversion integer,
-                    initvaluesversion integer,
-                    parameterversion integer,
-                    namepicture text,
-                    CONSTRAINT ex_pkey PRIMARY KEY(usubjid, exseq, extrt)
-                )
-                WITH(
-                    OIDS=FALSE
-                )
-                TABLESPACE pg_default;
-                """).format(sql.Identifier(NameOfModel)))
-
-        conn.commit()
-
-        """create pd table"""
-        cur.execute(sql.SQL("""
-                CREATE TABLE {0}.pd
-                (
-                    studyid text COLLATE pg_catalog."default",
-                    domain text COLLATE pg_catalog."default",
-                    usubjid text COLLATE pg_catalog."default" NOT NULL,
-                    pdseq integer NOT NULL,
-                    pdtestcd text COLLATE pg_catalog."default" NOT NULL,
-                    pdtest text COLLATE pg_catalog."default",
-                    pdorres double precision,
-                    pdorresu text COLLATE pg_catalog."default",
-                    pddtc double precision NOT NULL,
-                    co text COLLATE pg_catalog."default",
-                    CONSTRAINT pd_pkey PRIMARY KEY(usubjid, pdseq, pdtestcd, pddtc)
-                )
-                WITH(
-                    OIDS=FALSE
-                )
-                TABLESPACE pg_default;
-                """).format(sql.Identifier(NameOfModel)))
-
-        conn.commit()
+from sqlalchemy import create_engine
+from sqlalchemy import Column, String, JSON, DateTime, Integer, Enum
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.dialects.postgresql import UUID, ARRAY, REAL, DOUBLE_PRECISION
+import enum
+import datetime
 
 
-        cur.execute(sql.SQL("""
-                CREATE TABLE {0}.init_values
-                (
-                    seq integer NOT NULL,
-                    testcd text COLLATE pg_catalog."default" NOT NULL,
-                    test text COLLATE pg_catalog."default",
-                    orres double precision,
-                    orresu text COLLATE pg_catalog."default",
-                    co text COLLATE pg_catalog."default",
-                    CONSTRAINT init_values_pkey PRIMARY KEY (seq, testcd)
-                )
-                WITH (
-                    OIDS = FALSE
-                )
-                TABLESPACE pg_default;
-                """).format(sql.Identifier(NameOfModel)))
+db = create_engine('postgresql://postgres@db_postgres/simulation_results')
+base = declarative_base()
 
-        conn.commit()
+# all supported models as an enum, so that our database can work with that
+class Models(enum.Enum):
+    combined = 'combined_models'
+    hog = 'hog'
+    ion = 'ion'
+    volume = 'volume'
 
+#TODO: find a more meaningful name for this table, what does this json do?
+class JSON(base):
+    __tablename__ = 'json'
 
-        cur.execute(sql.SQL("""
-                CREATE TABLE {0}.parameter
-                (
-                    seq integer NOT NULL,
-                    testcd text COLLATE pg_catalog."default" NOT NULL,
-                    test text COLLATE pg_catalog."default",
-                    orres double precision,
-                    orresu text COLLATE pg_catalog."default",
-                    co text COLLATE pg_catalog."default",
-                    CONSTRAINT parameter_pkey PRIMARY KEY (seq, testcd)
-                )
-                WITH (
-                    OIDS = FALSE
-                )
-                TABLESPACE pg_default;
-                """).format(sql.Identifier(NameOfModel)))
+    seq = Column(Integer, primary_key=True)
+    uuid = Column(UUID(as_uuid=True), unique=True, nullable=False)
+    created_at = Column(DateTime, default=datetime.datetime.utcnow)
+    model = Column(Enum(Models))
+    model_version = Column(JSON)
+    adding_changes = Column(JSON)
+    deleting_changes = Column(JSON)
 
-        conn.commit()
+#TODO: again this name is rather non self explanatory, is that a table of my ex girlfriends?
+class Ex(base):
+    __tablename__ = 'ex'
 
-        cur.execute(sql.SQL("""
-                CREATE TABLE {0}.orresu_equations
-                (
-                    testcd text COLLATE pg_catalog."default" NOT NULL,
-                    test text COLLATE pg_catalog."default",
-                    orresu text COLLATE pg_catalog."default",
-                    CONSTRAINT orresu_equations_pkey PRIMARY KEY (testcd)
-                )
-                WITH (
-                    OIDS = FALSE
-                )
-                TABLESPACE pg_default;
-                """).format(sql.Identifier(NameOfModel)))
+    id = Column(Integer, primary_key=True)
+    uuid = Column(UUID(as_uuid=True), unique=True, nullable=False)
+    created_at = Column(DateTime, default=datetime.datetime.utcnow)
+    model = Column(Enum(Models))
+    studyid = Column(String)
+    domain = Column(String)
+    usubjid = Column(String)
+    exseq = Column(Integer, unique=True, nullable=False)
+    excat = Column(String)
+    extrt = Column(String, unique=True, nullable=False)
+    exdose = Column(REAL)
+    exdosu =Column(String)
+    exstdtc_array = Column(ARRAY(DOUBLE_PRECISION))
+    simulation_start = Column(DOUBLE_PRECISION)
+    simulation_stop = Column(DOUBLE_PRECISION)
+    co = Column(String)
+    model_version = Column(Integer)
+    initvalues_version = Column(Integer)
+    parameter_version = Column(String)
+    namepicture = Column(String)
 
-        conn.commit()
+#TODO: more meaningful name
+class Pd(base):
+    __tablename__ = 'pd'
 
+    id = Column(Integer, primary_key=True)
+    uuid = Column(UUID(as_uuid=True), unique=True, nullable=False)
+    created_at = Column(DateTime, default=datetime.datetime.utcnow)
+    model = Column(Enum(Models))
+    studyid = Column(String)
+    domain = Column(String)
+    usubjid = Column(String, unique=True)
+    pdseq = Column(Integer, unique=True, nullable=False)
+    pdtestcd = Column(String, unique=True, nullable=False)
+    pdtest = Column(String)
+    pdorres = Column(DOUBLE_PRECISION)
+    pdorresu = Column(String)
+    pddtc = Column(DOUBLE_PRECISION, unique=True)
+    co = Column(String)
 
+class InitialValue(base):
+    __tablename__ = 'initial_values'
 
-cur.close()
-conn.close()
+    seq = Column(Integer, primary_key=True)
+    uuid = Column(UUID(as_uuid=True), unique=True, nullable=False)
+    created_at = Column(DateTime, default=datetime.datetime.utcnow)
+    model = Column(Enum(Models))
+    testcd = Column(String, unique=True)
+    test = Column(String)
+    orres = Column(DOUBLE_PRECISION)
+    orresu = Column(String)
+    co = Column(String)
 
+class Parameter(base):
+    __tablename__ = 'parameters'
 
+    seq = Column(Integer, primary_key=True)
+    uuid = Column(UUID(as_uuid=True), unique=True, nullable=False)
+    created_at = Column(DateTime, default=datetime.datetime.utcnow)
+    model = Column(Enum(Models))
+    testcd = Column(String, unique=True, nullable=False)
+    test = Column(String)
+    orres = Column(DOUBLE_PRECISION)
+    orresu = Column(String)
+    co = Column(String)
+
+class OrresuEquation(base):
+    __tablename__ = 'orresu_equations'
+
+    id = Column(Integer, primary_key=True)
+    uuid = Column(UUID(as_uuid=True), unique=True, nullable=False)
+    created_at = Column(DateTime, default=datetime.datetime.utcnow)
+    model = Column(Enum(Models))
+    testcd = Column(String, nullable=False, unique=True)
+    test = Column(String)
+    orresu = Column(String)
+
+# Create tables if they do not yet exist.
+base.metadata.create_all(db)
 

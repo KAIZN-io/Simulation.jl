78c09f0e9fff5149f57608bcc1bf9487f5738ece
Jan Piotraschke <janpiotraschke@jans-imac.localdomain>
2019-01-28 12:23:57 +0100

    improved noise calculation; implemented seeding; still did not transform the ODE/SDE into parameterized functions but I found a tutorial for that on the official GitHub page

--- solver.jl
+++ solver.jl
@@ -17,25 +17,15 @@ function modelSystem(du,u,p,t)
  du[3] = u[1]*u[2] - p[3]*u[3]
 end
 
-# defining our noise as parameterized functions
-function noiseModelSystem(du,u,p,t)
-  du[1] = 3.0
-  du[2] = 3.0
-  du[3] = 3.0
-end
 
-# modelSystem = @ode_def_nohes modelSystem begin
-#   dx = σ*(y-x)
-#   dy = x*(ρ-z) - y
-#   dz = x*y - β*z
+# modelSystem = @ode_def_bare modelSystem begin
+#     dx = σ*(y-x)
+#     dy = x*(ρ-z) - y
+#     dz = x*y - β*z
 # end σ ρ β
 
-# noiseModelSystem = @ode_def_nohes noiseModelSystem begin
-#   dx = α
-#   dy = α
-#   dz = α
-# end α
-# α = 3.0
+# defining our noise as parameterized functions
+noiseModelSystem(du,u,p,t) = @.(du = 3.0)
 
 # define the initial Values --> vector 
 initialValues=[1.0,0.0,0.0]
@@ -46,14 +36,20 @@ parameter=[10.0,28.0,8/3]
 # time array (must be a tuple)
 timeRange = (0.0,2.0)
 
-# define the Problem 
-problem = SDEProblem(modelSystem, noiseModelSystem, initialValues, timeRange, parameter)
+# # change the Gaussian white noise
+# choosenNoise = WienerProcess(0.0,0.0,0.0)
+
+# define the Problem  -->  Gaussian white noise is default
+# set the seed for reproducing the same stochastic simulation
+problem = SDEProblem(modelSystem, noiseModelSystem, initialValues, timeRange, parameter, seed=1234)#, noise=choosenNoise)
 
 # solve the problem
 sol = solve(problem)
 
 # plot the solution
 plot(sol, linewidth=3,title="Solution of the SDE system")
+
+# 3d plot
 # plot(sol,vars=(1,2,3))
 
 

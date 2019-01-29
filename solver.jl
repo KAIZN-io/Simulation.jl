9d2e3a28f9fb30dfdd7c9166bed8a2fc51ee3aec
Jan Piotraschke <janpiotraschke@jans-imac.localdomain>
2019-01-29 13:50:15 +0100

    resolved a bug; note: there is a problem with the eval function

--- solver.jl
+++ solver.jl
@@ -5,34 +5,43 @@
 # import Pkg; Pkg.add("Plots")
 # import Pkg; Pkg.add("ParameterizedFunctions")
 # import Pkg; Pkg.add("BenchmarkTools")
-# import Pkg; Pkg.add("SymPy")
 
 using ParameterizedFunctions
 using DifferentialEquations, BenchmarkTools
 using Plots; plotly()
-using SymPy
 
-# define the equation as a string; maybe with the help of Sympy
-myString = "4 * x - y"
 
-# parse the string and make it an expression
-ex = Meta.parse(myString)
+# get the parameter for the model
+myParameterList = ["σ=10.0", "ρ=28.0", "β=8/3"]
+σ=10.0
+ρ=28.0
+β=8/3
+# make the parameter global 
+# for i = 1:size(myParameterList)[1]
+#     ex = Meta.parse(myParameterList[i])
 
-# defining our ODE system as parameterized functions
+#     # eval the expression in the function
+#     eval(ex)
+# end
+
+# define the equation as a string in a list
+myStringList = ["e = 3 * x - y", "c = 4 * y - x"]#, "dx = σ*(y-x) * e", "dy = x*(ρ-z) - y *c", "dz = x*y - β*z"]
+# myStringList = ["e = 3 * x - y", "c = 4 * y - x", "dx = σ*(y-x) * e", "dy = x*(ρ-z) - y *c", "dz = x*y - β*z"]
+
+# get the len of the array
+myArraySize = size(myStringList)[1]
+
 f_lorenz = @ode_def_bare LorenzSDE begin
-  dx = σ*(y-x) * eval(ex)
+  dx = σ*(y-x)
   dy = x*(ρ-z) - y
   dz = x*y - β*z
-end σ ρ β
+end 
 
 # defining our noise as parameterized functions
 noiseModelSystem(du,u,p,t) = @.(du = 3.0)
 
 # define the initial Values --> [1*m] vector 
 initialValues=[1.0;1.0;1.0]
-
-# define the parameters --> [m * n] matrix
-parameter=[10.0,28.0,8/3]
 
 # time array (must be a tuple)
 timeRange = (0.0,2.0)
@@ -42,7 +51,7 @@ timeRange = (0.0,2.0)
 
 # define the Problem  -->  Gaussian white noise is default
 # set the seed for reproducing the same stochastic simulation
-problem = SDEProblem(f_lorenz, noiseModelSystem, initialValues, timeRange, parameter, seed=1234)#, noise=choosenNoise)
+problem = SDEProblem(f_lorenz, noiseModelSystem, initialValues, timeRange)#, seed=1234)#, noise=choosenNoise)
 
 # solve the problem
 sol = solve(problem)
@@ -50,10 +59,8 @@ sol = solve(problem)
 # check how long it takes to solve the equation system 
 # @benchmark sol = solve(problem)#, save_everystep=false)
 
-
 # plot the solution
 plot(sol, linewidth=3,title="Solution of the SDE system")
 
 # # 3d plot
 # plot(sol,vars=(1,2,3))
-

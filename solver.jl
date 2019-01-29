10a1f07cbd0a6d40782ac5315e52542a9fe7329d
Jan Piotraschke <janpiotraschke@jans-imac.localdomain>
2019-01-29 11:52:30 +0100

    equation can be imported as a string now

--- solver.jl
+++ solver.jl
@@ -7,21 +7,20 @@
 # import Pkg; Pkg.add("BenchmarkTools")
 # import Pkg; Pkg.add("SymPy")
 
-
-
 using ParameterizedFunctions
 using DifferentialEquations, BenchmarkTools
 using Plots; plotly()
 using SymPy
 
-# # define the equation with the help of Sympy
-# # myString = "u[1]*u[2]"
-# ex = :(x*y)
-# #  equationMatrix = Sym(myString)
+# define the equation as a string; maybe with the help of Sympy
+myString = "4 * x - y"
+
+# parse the string and make it an expression
+ex = Meta.parse(myString)
 
 # defining our ODE system as parameterized functions
 f_lorenz = @ode_def_bare LorenzSDE begin
-  dx = σ*(y-x)
+  dx = σ*(y-x) * eval(ex)
   dy = x*(ρ-z) - y
   dz = x*y - β*z
 end σ ρ β
@@ -30,7 +29,7 @@ end σ ρ β
 noiseModelSystem(du,u,p,t) = @.(du = 3.0)
 
 # define the initial Values --> [1*m] vector 
-initialValues=[1.0;0.0;0.0]
+initialValues=[1.0;1.0;1.0]
 
 # define the parameters --> [m * n] matrix
 parameter=[10.0,28.0,8/3]
@@ -46,15 +45,15 @@ timeRange = (0.0,2.0)
 problem = SDEProblem(f_lorenz, noiseModelSystem, initialValues, timeRange, parameter, seed=1234)#, noise=choosenNoise)
 
 # solve the problem
-# sol = solve(problem)
+sol = solve(problem)
 
 # check how long it takes to solve the equation system 
-@benchmark sol = solve(problem)#, save_everystep=false)
+# @benchmark sol = solve(problem)#, save_everystep=false)
 
 
 # plot the solution
-# plot(sol, linewidth=3,title="Solution of the SDE system")
+plot(sol, linewidth=3,title="Solution of the SDE system")
 
 # # 3d plot
-# # plot(sol,vars=(1,2,3))
+# plot(sol,vars=(1,2,3))
 

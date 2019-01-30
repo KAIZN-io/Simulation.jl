253985fa1bd0b8ec1fb409d9d7114d33f129626d
Jan Piotraschke <janpiotraschke@jans-imac.localdomain>
2019-01-30 15:24:40 +0100

    further steps towards term knockout

--- solver.jl
+++ solver.jl
@@ -13,35 +13,53 @@ using Plots; plotly()
 
 # get the parameter for the model
 myParameterList = ["σ=10.0", "ρ=28.0", "β=8/3"]
-σ=10.0
-ρ=28.0
-β=8/3
-# make the parameter global 
-# for i = 1:size(myParameterList)[1]
-#     ex = Meta.parse(myParameterList[i])
 
-#     # eval the expression in the function
-#     eval(ex)
-# end
+# make the parameter global 
+for i = 1:size(myParameterList)[1]
+    ex = Meta.parse(myParameterList[i])
+    eval(ex)
+end
 
 # define the equation as a string in a list
-myStringList = ["e = 3 * x - y", "c = 4 * y - x"]#, "dx = σ*(y-x) * e", "dy = x*(ρ-z) - y *c", "dz = x*y - β*z"]
-# myStringList = ["e = 3 * x - y", "c = 4 * y - x", "dx = σ*(y-x) * e", "dy = x*(ρ-z) - y *c", "dz = x*y - β*z"]
+myStringList = ["e = 3 * x - y", "c = 4 * y - x"]
 
 # get the len of the array
 myArraySize = size(myStringList)[1]
 
+# matrix for test 
+variableMatrix = ["e"; "d"]
+termMatrix = ["+3*x" "-y"; "+4*y" "-x"]
+
+# myStringList = ["e = 3 * x - y", "c = 4 * y - x", "dx = σ*(y-x) * e", "dy = x*(ρ-z) - y *c", "dz = x*y - β*z"]
+
+
+# create activated term matrix 
+activatedTermMatrix = fill("",size(termMatrix)[1])
+for row = 1:size(termMatrix,2)  
+  for column = 1:size(termMatrix,1)
+    # add strings with '*' together 
+    activatedTermMatrix[row] *= termMatrix[row,column]
+    end
+end
+
+# NOTE: Further Work
+# # our matrix for the construction of a neuronal network
+# neuronalNetworkMatrix = [[1 0]; [1 1]]
+
+# # Hadamard product .* of matrix 
+# activatedTermMatrix = termMatrix .* neuronalNetworkMatrix
+
 f_lorenz = @ode_def_bare LorenzSDE begin
+  # for j = 1:myArraySize
+  #     ex = Meta.parse(myStringList[j])
+  #     eval(ex)
+  # end
+
   dx = σ*(y-x)
   dy = x*(ρ-z) - y
   dz = x*y - β*z
 end 
 
-termMatrix = [[1 2]; [3 4]]
-neuronalNetworkMatrix = [[1 0]; [1 1]]
-
-# Hadamard product .* of matrix 
-activatedTermMatrix = termMatrix .* neuronalNetworkMatrix
 
 # defining our noise as parameterized functions
 noiseModelSystem(du,u,p,t) = @.(du = 3.0)
@@ -60,13 +78,13 @@ timeRange = (0.0,2.0)
 problem = SDEProblem(f_lorenz, noiseModelSystem, initialValues, timeRange)#, seed=1234)#, noise=choosenNoise)
 
 # solve the problem
-# sol = solve(problem)
+sol = solve(problem)
 
 # check how long it takes to solve the equation system 
 # @benchmark sol = solve(problem)#, save_everystep=false)
 
 # plot the solution
-# plot(sol, linewidth=3,title="Solution of the SDE system")
+plot(sol, linewidth=3,title="Solution of the SDE system")
 
 # # 3d plot
 # plot(sol,vars=(1,2,3))

a71557969ab23069c4da7b98b6d0ed7d1627b55c
Jan-Luca Klees <email@janlucaklees.de>
2019-01-30 12:40:39 +0100

    Added logging configuration
    With this commit I added code to configure the logging for our project.
    With the logging configure, we can use it throughout the code.

--- web_interface/app.py
+++ web_interface/app.py
@@ -1,5 +1,22 @@
 from flask import Flask
+from logging.config import dictConfig
 
+"""Configure logging"""
+dictConfig({
+    'version': 1,
+    'formatters': {'default': {
+        'format': '[%(asctime)s] %(process)d - %(levelname)s in %(module)s: %(message)s',
+    }},
+    'handlers': {'wsgi': {
+        'class': 'logging.StreamHandler',
+        'stream': 'ext://flask.logging.wsgi_errors_stream',
+        'formatter': 'default'
+    }},
+    'root': {
+        'level': 'INFO',
+        'handlers': ['wsgi']
+    }
+})
 
 """initialize flask app"""
 # setup flask
@@ -9,4 +26,9 @@ app = Flask(
     static_url_path='/pictures',
     static_folder='SimulationPictures'
 )
+
+# initialize logger
+app.logger
+
+app.logger.info("Logger initialized")
 

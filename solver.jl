ff37e2b500163f45acae2901d8bd3632c5d47153
Jan Piotraschke <janpiotraschke@jans-imac.localdomain>
2019-01-31 10:48:03 +0100

    added functions for a dynamically creation of the term matrix; terms are now initially stored in nested list; one problem keeps exist: should I make the funtion @ parameterized or not?

--- solver.jl
+++ solver.jl
@@ -10,7 +10,6 @@ using ParameterizedFunctions
 using DifferentialEquations, BenchmarkTools
 using Plots; plotly()
 
-
 # get the parameter for the model
 myParameterList = ["σ=10.0", "ρ=28.0", "β=8/3"]
 
@@ -20,47 +19,78 @@ for i = 1:size(myParameterList)[1]
     eval(ex)
 end
 
-# define the equation as a string in a list
-myStringList = ["e = 3 * x - y", "c = 4 * y - x"]
+function getMaxTermCount(arrayTerms)
+  maxTermCount = 0
+  for row in eachindex(arrayTerms)
+    y = arrayTerms[row]
+    arraySize = size(arrayTerms[row])[1]
+    if arraySize > maxTermCount
+      maxTermCount = arraySize
+    end
+  end
+  return maxTermCount
+end
+
+function fillTermMatrix(fillMatrix, arrayTerms)
+  for row in eachindex(arrayTerms)
+    y = arrayTerms[row]
+    for column in eachindex(y)
+      fillMatrix[row,column] = y[column]
+    end
+  end  
+  return fillMatrix 
+end
+
+# matrix with the variables name
+variableMatrix = ["e"; "d"]
+# variableMatrix = ["e"; "d"; "du[1]"; "du[2]"; "du[3]"]
 
 # get the len of the array
-myArraySize = size(myStringList)[1]
+myArraySize = size(variableMatrix)[1]
 
-# matrix for test 
-variableMatrix = ["e"; "d"]
-termMatrix = ["+3*x" "-2*y"; "+4*y" "-x"]
+# array of arrays 
+inputTermArray = Array[["+3*x", "-2*y"], ["+4*y", "-x"]]
+# inputTermArray = Array[["+3*x", "-2*y"], ["+4*y", "-x"], ["+σ*u[2]", "-σ*u[1]"], ["+u[1]*ρ" ,"-u[1]*u[3]" ,"- u[2]"], ["+u[1]*u[2]" ,"- β*u[3]"]]
 
-# TODO: make the neuronal network matrix dynamically
-neuronalNetworkMatrix = [[0 1]; [1 1]]
+# get the maximal term count
+maxTermCount = getMaxTermCount(inputTermArray)
+
+# create the generel term matrix
+emptyTermMatrix = fill("", myArraySize, maxTermCount)
+
+# fill the term matrix with the terms
+termMatrix = fillTermMatrix(emptyTermMatrix,inputTermArray)
+
+# create the neuronal network matrix filled with ones
+neuronalNetworkMatrix = ones(myArraySize,maxTermCount)
 
 # create activated term matrix 
 activatedTermMatrix = fill("",size(termMatrix)[1])
-for row = 1:size(termMatrix,2)  
-  for column = 1:size(termMatrix,1)
-    # if the term is allowed
+for row in 1:myArraySize
+  for column in 1:maxTermCount
     if neuronalNetworkMatrix[row,column] == 1
-      # add strings with '*' together  
-      activatedTermMatrix[row] *= termMatrix[row,column]
+      # add strings with '*' together
+      activatedTermMatrix[row,1] *= termMatrix[row,column]
     end
   end
-end
+end  
 
 # re-unit the variable matrix with their terms and precompile the equations
 expressionMatrix = [Meta.parse(string(variableMatrix[i], "=" ,activatedTermMatrix[i])) for i in 1:myArraySize]
 
-# testX = ["dx = σ*(y-x)"; "dy = x*(ρ-z)" ; "dz = x*y - β*z"]
-# expressionMatrixTest = [Meta.parse(testX[i]) for i in 1:3]
-
 function parameterized_lorenz(du,u,p,t)
+
   global x,y,z = u
 
   for j = 1:myArraySize
     eval(expressionMatrix[j])
   end
-
+  # dx = σ*(y-x) * e
+  # dy = x*(ρ-z) - y *d 
+  # dz = x*y - β*z
   # for better performance 
-  du[1] = σ*(u[2]-u[1])
-  du[2] = u[1]*(ρ-u[3]) - u[2]
+  du[1] = σ*u[2]-σ*u[1]
+  du[2] = u[1]*ρ-u[1]*u[3] - u[2]
   du[3] = u[1]*u[2] - β*u[3]
 end
 
@@ -80,13 +110,13 @@ timeRange = (0.0,2.0)
 # set the seed for reproducing the same stochastic simulation
 problem = SDEProblem(parameterized_lorenz, noiseModelSystem, initialValues, timeRange)#, seed=1234)#, noise=choosenNoise)
 
-# solve the problem
+# # solve the problem
 sol = solve(problem)
 
-# check how long it takes to solve the equation system 
+# # check how long it takes to solve the equation system 
 # @benchmark sol = solve(problem)#, save_everystep=false)
 
-# plot the solution
+# # plot the solution
 plot(sol, linewidth=3,title="Solution of the SDE system")
 
 # # 3d plot

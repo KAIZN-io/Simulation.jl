5ceee2bef63d310217a30ad1c165094adc7ea364
Jan-Luca Klees <email@janlucaklees.de>
2019-02-08 11:58:22 +0100

    Moved what was SimulationManager intot the web package
    The SimulationManager no longer manages simulations as he used to pre
    task-queue. It is now just a module containing some functions for the web
    interface to query simulations and schedule new ones. So it has no need
    staying in the root dir.

--- web/routings.py
+++ web/routings.py
@@ -1,9 +1,9 @@
 from flask import Blueprint, render_template, redirect
 
-import simulation_manager as sm
 import message_queue as mq
-from simulation_form import SimulationForm
 from db import sessionScope
+from web.simulation import getSimulationFromFormData, getScheduledSimulations, getFinishedSimulations
+from web.form import SimulationForm
 
 
 routes = Blueprint('routes', __name__)
@@ -19,7 +19,7 @@ def start():
     if form.validate_on_submit():
 
         with sessionScope() as session:
-            simulation = sm.getSimulationFromFormData(session, form.data)
+            simulation = getSimulationFromFormData(session, form.data)
             session.commit()
             mq.scheduleSimulation(simulation)
 
@@ -36,8 +36,8 @@ def start():
 def running():
 
     with sessionScope() as session:
-        scheduled_simulations = sm.getScheduledSimulations(session)
-        finished_simulations = sm.getFinishedSimulations(session)
+        scheduled_simulations = getScheduledSimulations(session)
+        finished_simulations = getFinishedSimulations(session)
 
         return render_template(
             'simulation/overview_active.html',
